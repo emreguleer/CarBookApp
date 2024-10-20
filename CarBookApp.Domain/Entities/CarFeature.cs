87dd4708d380476ab6ead5b53e6103d6bd67638f
Emre Güler <guler.emre.99@gmail.com>
2024-10-20 23:25:18 +0300

    Add project files.

---
+++ CarBookApp.Domain/Entities/CarFeature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApp.Domain.Entities
+{
+    public class CarFeature
+    {
+        public int CarFeatureId { get; set; }
+        public int CarId { get; set; }
+        public virtual Car Car { get; set; }
+        public int FeatureId { get; set; }
+        public virtual Feature Feature { get; set; }
+        public bool Available { get; set; }
+    }
+}
